0c02b37bb4f942caf7392f9043a0b984f15efdfe
Pedro Henrique Ribeiro de Azevedo <62411379+PedroHy@users.noreply.github.com>
2024-03-11 16:19:01 -0300

    initial architeture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +10,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  constructor(private router: Router){}
+
+  navigateToHome = ()=>this.router.navigateByUrl('')
+  
+  navigateToAbout = ()=>this.router.navigateByUrl('/about')
+  
+  navigateToProjects = ()=>this.router.navigateByUrl('/projects')
+  
+  navigateToCertifications = ()=>this.router.navigateByUrl('/certifications')
+  
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CertificationsComponent } from './components/certifications/certifications.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'projects',
+        component: ProjectsComponent
+    },
+    {
+        path: 'certifications',
+        component: CertificationsComponent
+    }
+];
