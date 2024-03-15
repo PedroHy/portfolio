c26f663b9bfd0551a0e7147a989809a77ae19415
Pedro Henrique Ribeiro de Azevedo <62411379+PedroHy@users.noreply.github.com>
2024-03-15 12:01:12 -0300

    adding projects

---
+++ src/assets/api/projects.ts
@@ -0,0 +1,39 @@
+const projects = [
+    {
+        "title": "Disney Plus Clone",
+        "description": "Esse foi um dos meus primeiros projetos utilizando React e Tailwind onde simplesmente eu abri o site do Disney plus e coloquei como meta que iria fazer um clone da interface sem tutoriais nem nada do genêro.",
+        "techs": "React, Tailwind, Router, Redux",
+        "img": "../../../assets/api/images/disney.png",
+        "video": "../../../assets/api/videos/disney.mp4"
+    },
+    {
+        "title": "Departament Manager",
+        "description": "Basicamente o projeto consiste em uma aplicação que deve gerenciar os departamentos de uma empresa com seus respectivos funcionários. Um departamento pode ter vários funcionários, mas um funcionário só pode pertencer a um único departamento. Esse foi meu primeiro projeto que utilizei Angular, já havia usado o ASP.NET na faculdade mas nunca tinha desenvolvido um projeto pessoal utilizando ele, esse tambem foi o primeiro projeto que programei o front, api e integrei com Firebase Storage e um banco mySQL. ",
+        "techs": "ASP.NET, Angular, Firebase, MySQL",
+        "img": "",
+        "video": "../../../assets/api/videos/departament.mp4"
+    },
+    {
+        "title": "Investing",
+        "description": "Meu primeiro projeto integrando uma API externa que desenvolvi em collab com meu amigo Elias Pinheiro. Basicamente consumimos uma API que nos retornava a cotação de ações da B3 e baseado nessas informações montamos esse dashboard onde é possivel ver a variação de preços de determinada ação selecionada.",
+        "techs": "HTML, CSS, JS",
+        "img": "",
+        "video": "../../../assets/api/videos/investing.mp4"
+    },
+    {
+        "title": "EcoEletro",
+        "description": "basicamente o EcoEletro consiste em uma aplicação WEB onde um usuario pode vizualizar pontos de de descarte de lixo eletrônico mais próximo dele, e uma empresa ou local que deseja ser um ponto de coleta pode se cadastrar e adicionar o seu endereço no site. Esse projeto foi útil para aprender sobre como funciona um fluxo de autenticação e rotas seguras no Angular.",
+        "techs": "Angular, Firebase, Tailwind",
+        "img": "",
+        "video": ""
+    },
+    {
+        "title": "Pizzaria",
+        "description": "",
+        "techs": "React Native",
+        "img": "",
+        "video": "../../../assets/api/videos/pizza.mp4"
+    }
+]
+
+export default projects
